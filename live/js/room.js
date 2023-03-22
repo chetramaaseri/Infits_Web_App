d26cbbb207e806a7e372ed39fe7cc69e88103500
its7080 <AM7059141480@gmail.com>
2023-03-22 13:51:39 +0530

    Live feature

--- live/js/room.js
+++ live/js/room.js
@@ -1,4 +1,4 @@
-let messagesContainer = document.getElementById('messages');
+let messagesContainer = document.getElementById('_messages');
 messagesContainer.scrollTop = messagesContainer.scrollHeight;
 
 const memberContainer = document.getElementById('members__container');
@@ -38,16 +38,16 @@ let userIdInDisplayFrame = null;
 let expandVideoFrame = (e) => {
 
   let child = displayFrame.children[0]
-  if(child){
-      document.getElementById('streams__container').appendChild(child)
+  if (child) {
+    document.getElementById('streams__container').appendChild(child)
   }
 
   displayFrame.style.display = 'block'
   displayFrame.appendChild(e.currentTarget)
   userIdInDisplayFrame = e.currentTarget.id
 
-  for(let i = 0; videoFrames.length > i; i++){
-    if(videoFrames[i].id != userIdInDisplayFrame){
+  for (let i = 0; videoFrames.length > i; i++) {
+    if (videoFrames[i].id != userIdInDisplayFrame) {
       videoFrames[i].style.height = '100px'
       videoFrames[i].style.width = '100px'
     }
@@ -55,21 +55,21 @@ let expandVideoFrame = (e) => {
 
 }
 
-for(let i = 0; videoFrames.length > i; i++){
+for (let i = 0; videoFrames.length > i; i++) {
   videoFrames[i].addEventListener('click', expandVideoFrame)
 }
 
 
 let hideDisplayFrame = () => {
-    userIdInDisplayFrame = null
-    displayFrame.style.display = null
+  userIdInDisplayFrame = null
+  displayFrame.style.display = null
 
-    let child = displayFrame.children[0]
-    document.getElementById('streams__container').appendChild(child)
+  let child = displayFrame.children[0]
+  document.getElementById('streams__container').appendChild(child)
 
-    for(let i = 0; videoFrames.length > i; i++){
-      videoFrames[i].style.height = '300px'
-      videoFrames[i].style.width = '300px'
+  for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].style.height = '300px'
+    videoFrames[i].style.width = '300px'
   }
 }
 
