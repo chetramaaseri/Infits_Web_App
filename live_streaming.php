3d07c88ff291f78a663ce9a132c90547f003b9f7
its7080 <AM7059141480@gmail.com>
2023-03-29 15:33:06 +0530

    Revert "style"
    This reverts commit b3ea98cfd726ddf15c0379b9477809b6eaa43309.

--- live_streaming.php
+++ live_streaming.php
@@ -315,9 +315,6 @@ height: 400px;
   align-items: center;
   margin-top: 25px;
   margin-bottom: 20px;
-
-  height: 440px;
-
 }
 
 .video__container{
@@ -369,7 +366,6 @@ width: 700px;
   display: none; */
   gap: 1rem;
   /* transform: translateX(-50%); */
-  display: none;
 }
 
 .stream__actions a,
