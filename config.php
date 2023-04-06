14e1d2c09ef88fa930795b63f7269867feb658bc
its7080 <AM7059141480@gmail.com>
2023-04-06 23:00:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'infits');
+define('DB_SERVER', 'db4free.net');
+define('DB_PORT', '3306');
+define('DB_USERNAME', 'infits_free_test');
+define('DB_PASSWORD', 'EH6.mqRb9QBdY.U');
+define('DB_NAME', 'infits_db');
+
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'infits');
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 //Check the connection
